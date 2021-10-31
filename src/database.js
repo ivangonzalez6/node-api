c2ed20448ecc5afb341185700fc9c772a43ff815
ivangonzalez6 <66489048+ivangonzalez6@users.noreply.github.com>
2021-10-31 13:46:46 -0600

    edit vercel.json

--- src/database.js
+++ src/database.js
@@ -1,18 +1,18 @@
-const mysql= require('mysql');
-const mysqlConnection=mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'unwell',
-database: 'company'
+const mysql = require("mysql");
+const mysqlConnection = mysql.createConnection({
+  host: "sql5.freemysqlhosting.net",
+  user: "sql5448044",
+  password: "nW1SwRP8Ja",
+  database: "sql5448044",
 });
 
-mysqlConnection.connect(function(err){
-if(err){
+mysqlConnection.connect(function (err) {
+  if (err) {
     console.log(err);
     return;
-}else{
-    console.log('DB is connected');
-}
+  } else {
+    console.log("DB is connected");
+  }
 });
 
 module.exports = mysqlConnection;
