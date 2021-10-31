38a0aa92e4b674808949c8114411d0e9b8214a11
ivangonzalez6 <66489048+ivangonzalez6@users.noreply.github.com>
2021-10-31 14:09:26 -0600

    index moved

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 
 //Routes
-app.use(require('./routes/employees'));
+app.use(require('./src/routes/employees'));
 
 //Starting server
 app.listen(app.get("port"), () => {
